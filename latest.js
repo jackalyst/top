de8e2a9cea9e1a7f9a8f99b2a5f7d4581e91b9f2
Jack Matier <self@jackalyst.com>
2012-10-20 04:23:00 -0600

    Testing GIT on windows.

--- latest.js
+++ latest.js
@@ -1,11 +1,9 @@
+// Contributing a useless change to a file.
 // Testing out stylesheet detection.
 window.onload = function() {
 	var stylesheets = document.styleSheets;
 
 	// IE uses 'rules'.
-
 	console.log("There are "+stylesheets[0].cssRules.length+ " declarations in the first stylesheet.");
-
-
 
 }
