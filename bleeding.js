b3b944af3bcfc02da553e8f26fdaee800d06b426
Jack Matier <self@jackalyst.com>
2013-12-08 11:06:18 -0700

    Cleanup, outline JS. Still Unstable.

--- bleeding.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-	var stylesheets = document.styleSheets;
-
-	console.log("There are "+stylesheets[0].cssRules.length+ " declarations in the first stylesheet.");
-}
---
+++ top.bleeding.js
@@ -0,0 +1,13 @@
+/*
+ * Self executing function to run at end of page!
+ */
+(function() {
+	var stylesheets = document.styleSheets;
+
+	// Run through each CSS element.
+		// Look for .onclick, .onmousedown
+		// assign handlers to toggle classes.
+
+	// Run through DOM
+		//
+})();
